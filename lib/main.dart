9480cbe90f5ced95d42b10792c831eab2cc4dffa
Wilson <waramos97@gmail.com>
2020-02-29 21:37:06 -0300

    Aplicativo OK

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:agenda_de_contatos/ui/contact_page.dart';
+import 'package:flutter/material.dart';
+import 'package:agenda_de_contatos/ui/home_page.dart';
+
+void main(){
+  runApp(
+    MaterialApp(
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    )
+  );
+}
